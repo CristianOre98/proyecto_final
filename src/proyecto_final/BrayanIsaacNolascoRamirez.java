e13036a414d029a27cd77b48750ad947e066574d
Nolasco <Nolasco@DESKTOP-1IC3J0M>
2020-05-14 15:52:12 -0600

    Eh agregado el metodo main y el codigo para hacer una multiplicacion

--- src/proyecto_final/BrayanIsaacNolascoRamirez.java
+++ src/proyecto_final/BrayanIsaacNolascoRamirez.java
@@ -7,6 +7,27 @@ import java.util.Scanner;
  * @author Nolasco
  */
 public class BrayanIsaacNolascoRamirez {
-   
+   public static void main(String[] args) {
+    
+    Scanner leer = new Scanner(System.in);
+      
+    double numero1, numero2, M;
+    
+        System.out.println("----MULTIPLICACION-----");
+        
+        System.out.println("Digite el primer valor");
+        numero1 = leer.nextDouble();
+        
+         System.out.println("Digite el segundo valor");
+        numero2 = leer.nextDouble();
+        
+        M = numero1 * numero2;
+        
+        System.out.println("-----------------------");
+        System.out.println("Resultado" + M);
+        System.out.println("-----------------------");
+        
+        
+    }     
         
 }
