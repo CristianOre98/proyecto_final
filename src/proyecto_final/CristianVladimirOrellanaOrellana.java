c177615889a88f22f17883ca71188b801afe8331
CristianOre98 <crisvlaore98@gmail.com>
2020-05-14 15:54:45 -0600

    Revert "Revert "Revert "agrege el metodo main y el codigo para hacer una suma"""
    This reverts commit 3cb4a9320c813f5e6908949ee9cb8749bb780b92.

--- src/proyecto_final/CristianVladimirOrellanaOrellana.java
+++ src/proyecto_final/CristianVladimirOrellanaOrellana.java
@@ -4,26 +4,11 @@
  * and open the template in the editor.
  */
 package proyecto_final;
-import java.util.Scanner;
 
+/**
+ *
+ * @author Iván Pérez
+ */
 public class CristianVladimirOrellanaOrellana {
-        public static void main(String[] args) {
-        
-        Scanner leer = new Scanner(System.in);
-        
-    double n1, n2, result;
     
-        System.out.println("******* Suma ***********");
-        System.out.println("Ingrese el primer numero");
-        n1 = leer.nextDouble();
-        System.out.println("Ingrese el primer numero");
-        n2 = leer.nextDouble();
-        
-        result = n1 * n2;
-        System.out.println("*************************");
-        System.out.println("**** resultado de la suma es: "+result);
-        System.out.println("*************************");
-    
-    
-    }
 }
