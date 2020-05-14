57d9719f7381f10953f529efbb30191c0d5a79c1
Douglasayala <douglasayal a612@gmail.com>
2020-05-14 15:46:05 -0600

    He agregado la operacion resta

--- src/proyecto_final/Dougla_Josue_Ayala_Cordova.java
+++ src/proyecto_final/Dougla_Josue_Ayala_Cordova.java
@@ -3,5 +3,27 @@ package proyecto_final;
 import java.util.Scanner;
 
 public class Dougla_Josue_Ayala_Cordova {
+    public static void main(String[] args) {
+    
+    Scanner leer = new Scanner(System.in);
+      
+    double numero1, numero2, R;
+    
+        System.out.println("-----RESTA-----");
+        
+        System.out.println("Digite el primer valor");
+        numero1 = leer.nextDouble();
+        
+         System.out.println("Digite el segundo valor");
+        numero2 = leer.nextDouble();
+        
+        R = numero1 - numero2;
+        
+        System.out.println("-----------------------");
+        System.out.println("Resultado" + R);
+        System.out.println("-----------------------");
+        
+        
+    }     
     
 }
