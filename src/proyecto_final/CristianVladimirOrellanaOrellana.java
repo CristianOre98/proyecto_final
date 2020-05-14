811d67026e25ec15e2d1b0faecd48caedb1eec18
CristianOre98 <crisvlaore98@gmail.com>
2020-05-14 15:45:57 -0600

    agrege el metodo main y el codigo para hacer una suma

--- src/proyecto_final/CristianVladimirOrellanaOrellana.java
+++ src/proyecto_final/CristianVladimirOrellanaOrellana.java
@@ -5,10 +5,27 @@
  */
 package proyecto_final;
 
-/**
- *
- * @author Iván Pérez
- */
+import java.util.Scanner;
+
 public class CristianVladimirOrellanaOrellana {
+    public static void main(String[] args) {
+        
+        Scanner leer = new Scanner(System.in);
+        
+    double n1, n2, result;
+    
+        System.out.println("******* Suma ***********");
+        System.out.println("Ingrese el primer numero");
+        n1 = leer.nextDouble();
+        System.out.println("Ingrese el primer numero");
+        n2 = leer.nextDouble();
+        
+        result = n1 * n2;
+        System.out.println("*************************");
+        System.out.println("**** resultado de la suma es: "+result);
+        System.out.println("*************************");
+    
+    
+    }
     
 }
