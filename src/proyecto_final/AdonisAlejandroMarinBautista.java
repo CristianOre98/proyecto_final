85e1a4d3722c8d549a77d6755cbd60cf3cec371b
Adonis <adonisbautista39@gmail.com>
2020-05-14 18:25:20 -0700

    Eh agregado codigo para la clase dividir

--- src/proyecto_final/AdonisAlejandroMarinBautista.java
+++ src/proyecto_final/AdonisAlejandroMarinBautista.java
@@ -10,5 +10,26 @@ import java.util.Scanner;
  * @author Alejandro
  */
 public class AdonisAlejandroMarinBautista {
+     public static void main(String[] args) {
     
+    Scanner leer = new Scanner(System.in);
+      
+    double numero1, numero2, D;
+    
+        System.out.println("----DIVISIÒN-----");
+        
+        System.out.println("Digite el primer valor");
+        numero1 = leer.nextDouble();
+        
+         System.out.println("Digite el segundo valor");
+        numero2 = leer.nextDouble();
+        
+        D = numero1 / numero2;
+        
+        System.out.println("-----------------------");
+        System.out.println("Resultado" + D);
+        System.out.println("-----------------------");
+        
+        
+    }     
 }
