213c45e788e462f538f357d614db0821161274e9
CristianOre98 <crisvlaore98@gmail.com>
2020-05-16 12:27:57 -0600

    he Creado README.md

--- src/Operaciones_basicas/AdonisAlejandroMarinBautista.java
+++ src/Operaciones_basicas/AdonisAlejandroMarinBautista.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package proyecto_final;
+package Operaciones_basicas;
 import java.util.Scanner;
 /**
  *
